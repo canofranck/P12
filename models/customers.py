2643dc9838e8c755568a5f79009bf738d3027b89
canofranck <112980088+canofranck@users.noreply.github.com>
2024-06-07 16:22:58 +0200

    Update customers.py

--- models/customers.py
+++ models/customers.py
@@ -1,57 +1,79 @@
+from typing import List
+import uuid
+import bcrypt
 from sqlalchemy.orm import relationship, declarative_base
-from sqlalchemy import (
-    CHAR,
-    VARCHAR,
-    Column,
-    Integer,
-    String,
-    ForeignKey,
-    UUID,
-    DateTime,
-)
+from sqlalchemy import CHAR, VARCHAR, Column, Integer, String, UUID, Enum
+import enum
+
+from models.customers import Customer
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
-from datetime import datetime
-import uuid
-
 
 Base = declarative_base()
 
 
-class Customer(Base):
+class UserPermission:
+    pass
 
-    __tablename__ = "customers"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    first_name = Column(String(50), nullable=False)
-    last_name = Column(String(50), nullable=False)
-    email = Column(String(50), unique=True, index=True)
-    phone_number = Column(String(20), unique=True, index=True)
-    compagny_name = Column(String(100), unique=False, index=True)
-    creation_date = Column(DateTime, default=datetime.now)
-    last_contact_date = Column(DateTime, default=datetime.now)
-    # sales_id = Column(Integer(), ForeignKey("users.id"), nullable=True)
-    # sales = relationship("User", back_populates="customers")
-    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
-    # user = relationship("User", back_populates="contract")
+class UserRole(enum.Enum):
+    MANAGER = 1
+    SALES = 2
+    SUPPORT = 3
+    ADMIN = 4
+
+    def __str__(self):
+        return self.name
+
+
+def create_hash_password(password: str) -> str:
+    bytes = password.encode("utf-8")
+    salt = bcrypt.gensalt()
+    hash_password = bcrypt.hashpw(bytes, salt)
+    return hash_password.decode("utf-8")
+
+
+class User(Base):
+
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    username = Column(String(50), nullable=False, unique=True)
+    password = Column(String(60), nullable=False)
+    full_name = Column(String(100), nullable=False)
+    email = Column(String(60), unique=True, nullable=False)
+    phone_number = Column(String(20), nullable=False)
+    role = Column(Enum(UserRole), nullable=False)
+
+    contracts_map: Mapped[List["Contract"]] = relationship(
+        back_populates="user", cascade="all, delete-orphan"
+    )
+
+    # sales = relationship("Customer", back_populates="user")
+    # contracts: Mapped[List["Contract"]] = relationship(back_populates="user")
 
     def __init__(
         self,
-        first_name,
-        last_name,
+        username,
+        password,
+        full_name,
         email,
         phone_number,
-        compagny_name,
-        sales_id,
+        role,
     ):
+        hashed_password = create_hash_password(password)
 
-        self.first_name = first_name
-        self.last_name = last_name
+        self.username = username
+        self.full_name = full_name
+        self.role = role
         self.email = email
         self.phone_number = phone_number
-        self.compagny_name = compagny_name
-
-        self.sales_id = sales_id
+        self.password = hashed_password
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name} ({self.email})"
+        return (
+            f"User Name: {self.username} "
+            f"Full Name: {self.last_name} "
+            f"Email: {self.email} "
+            f"Role: {self.role} "
+        )
